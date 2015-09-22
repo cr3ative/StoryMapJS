9fe0fbcbebd3023fef44db780bf95b1fac8f6bb7
Paul Curry <github@cr3ation.co.uk>
2015-09-22 11:28:12 +0100

    Allow build files to load cross-origin
    So you can develop on http://dev.contoso.com/story/index.html and point
    assets at http://localhost:5000/build/js/storymap.js for example

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from __future__ import division
 from flask import Flask, request, session, redirect, url_for, \
-    render_template, jsonify, abort
+    render_template, jsonify, abort, make_response, current_app
 from collections import defaultdict
 import math
 import os
@@ -15,6 +15,49 @@ from functools import wraps
 import urllib
 from urlparse import urlparse
 
+from datetime import timedelta
+from functools import update_wrapper
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 # Import settings module
 if __name__ == "__main__":
@@ -851,6 +894,7 @@ compiled_dir = os.path.join(settings.PROJECT_ROOT, 'compiled')
 templates_dir = os.path.join(settings.PROJECT_ROOT, 'compiled/templates')
 
 @app.route('/build/<path:path>')
+@crossdomain(origin='*')
 def catch_build(path):
     return send_from_directory(build_dir, path)
 
